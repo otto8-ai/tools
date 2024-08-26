479331f5130e7369c0f56fa21e633e5f2532d631
Grant Linville <grant@acorn.io>
2024-08-26 15:23:09 -0400

    remove unused util functions
    Signed-off-by: Grant Linville <grant@acorn.io>

--- apis/outlook/calendar/code/pkg/util/util.go
+++ apis/outlook/calendar/code/pkg/util/util.go
@@ -1,7 +1,5 @@
 package util
 
-import "time"
-
 func Ptr[T any](v T) *T {
 	return &v
 }
@@ -19,28 +17,4 @@ func Map[T, U any](arr []T, f func(T) U) []U {
 		out = append(out, f(v))
 	}
 	return out
-}
-
-func Dedupe[T any, U comparable](arr []T, id func(T) U) []T {
-	seen := make(map[U]bool)
-	var out []T
-	for _, v := range arr {
-		i := id(v)
-		if !seen[i] {
-			seen[i] = true
-			out = append(out, v)
-		}
-	}
-	return out
-}
-
-func TimeFilter(start, end *time.Time) string {
-	if start != nil && end != nil {
-		return "start/dateTime ge " + start.Format(time.RFC3339) + " and end/dateTime le " + end.Format(time.RFC3339)
-	} else if start != nil {
-		return "start/dateTime ge " + start.Format(time.RFC3339)
-	} else if end != nil {
-		return "end/dateTime le " + end.Format(time.RFC3339)
-	}
-	return ""
 }
