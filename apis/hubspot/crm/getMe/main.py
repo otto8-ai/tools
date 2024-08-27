2ec8f424e97c62559d4de7d6ce9b34911369db98
Taylor Price <tayworm@gmail.com>
2024-08-27 13:42:53 -0700

    chore: retrieve ownerId
    Signed-off-by: Taylor Price <tayworm@gmail.com>

--- apis/hubspot/crm/getMe/main.py
+++ apis/hubspot/crm/getMe/main.py
@@ -3,10 +3,14 @@ import os
 from hubspot import HubSpot
 
 token = os.getenv("GPTSCRIPT_API_HUBAPI_COM_BEARER_TOKEN")
-client = HubSpot()
+client = HubSpot(access_token=token)
 
 try:
     user = client.auth.oauth.access_tokens_api.get(token)
-    print(f"Current user is {user.user}. User Id is {user.user_id}.")
+    print(f"Current user is {user.user}. UserId is {user.user_id}.")
 except Exception as e:
     print(f"Error getting current user information: {e}")
+
+
+owners = client.crm.owners.owners_api.get_page(email=user.user)
+print(f"OwnerId is {owners.results[0].id}. Use this to look up ownership associations.")
