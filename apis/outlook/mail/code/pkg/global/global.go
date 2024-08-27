252e5b76e947a181f578f7a5befd3f8604327e32
Grant Linville <grant@acorn.io>
2024-08-27 15:05:06 -0400

    add scopes
    Signed-off-by: Grant Linville <grant@acorn.io>

--- apis/outlook/mail/code/pkg/global/global.go
+++ apis/outlook/mail/code/pkg/global/global.go
@@ -3,6 +3,6 @@ package global
 const CredentialEnv = "GPTSCRIPT_GRAPH_MICROSOFT_COM_BEARER_TOKEN"
 
 var (
-	ReadOnlyScopes = []string{"TODO"}
-	AllScopes      = []string{"TODO"}
+	ReadOnlyScopes = []string{"Mail.Read", "User.Read"}
+	AllScopes      = []string{"Mail.Read", "Mail.ReadWrite", "Mail.Send", "User.Read"}
 )
