fde56d20baa6d693b2e1a93e67af83225d02c1d6
Taylor Price <tayworm@gmail.com>
2024-09-24 11:01:29 -0700

    chore: youtube transcriber tool
    Signed-off-by: Taylor Price <tayworm@gmail.com>

---
+++ apis/google/youtube/code/transcriber.py
@@ -0,0 +1,23 @@
+import os
+import urllib.parse as urlparse
+from urllib.parse import parse_qs
+
+from youtube_transcript_api import YouTubeTranscriptApi
+
+video_url = os.getenv('VIDEO_URL')
+if video_url is None:
+    raise ValueError("Error: video_url must be set")
+
+parsed = urlparse.urlparse(video_url)
+if parsed.netloc == 'www.youtube.com':
+    video_id = parse_qs(parsed.query)['v'][0]
+elif parsed.netloc == 'youtu.be':
+    video_id = parsed.path.strip('/').split('/')[0]
+else:
+    raise ValueError(f"Invalid YouTube URL: {video_url}")
+
+transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+
+transcript_text = ' '.join(item['text'] for item in transcript_list)
+
+print(transcript_text)
