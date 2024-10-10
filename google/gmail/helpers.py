3f441188430c66423934596645d00df8be4fa132
Nick Hale <4175918+njhale@users.noreply.github.com>
2024-10-10 16:08:50 -0400

    enhance: make gmail tools self-contained
    - Share tool dependencies so that individual tools can discover required
      arguments
    - Add List Drafts and Draft Context tools
    - Extend Read Email tool to handle reading drafts
    - Factor out some shared logic
    
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- google/gmail/helpers.py
+++ google/gmail/helpers.py
@@ -76,15 +76,121 @@ def display_list_messages(service, messages: list):
                .execute())
 
         msg_id = msg['id']
-        subject = None
-        sender = None
-        date = datetime.fromtimestamp(int(msg['internalDate']) / 1000, timezone.utc).astimezone().strftime(
-            '%Y-%m-%d %H:%M:%S')
+        subject, sender, to, cc, bcc, date = extract_message_headers(msg)
+        print(f"ID: {msg_id} From: {sender}, Subject: {subject}, To: {to}, CC: {cc}, Bcc: {bcc}, Received: {date}")
 
-        for header in msg['payload']['headers']:
+
+def list_drafts(service, max_results=None):
+    all_drafts = []
+    next_page_token = None
+    try:
+        while True:
+            if next_page_token:
+                results = service.users().drafts().list(userId='me', pageToken=next_page_token, maxResults=10).execute()
+            else:
+                results = service.users().drafts().list(userId='me', maxResults=10).execute()
+ 
+            drafts = results.get('drafts', [])
+            if not drafts:
+                break
+
+            all_drafts.extend(drafts)
+            if max_results is not None and len(all_drafts) >= max_results:
+                break
+
+            next_page_token = results.get('nextPageToken')
+            if not next_page_token:
+                break
+
+        display_list_drafts(service, all_drafts)
+
+    except HttpError as err:
+        print(f"An error occurred: {err}")
+
+
+def display_list_drafts(service, drafts: list):
+    print('Drafts:')
+    for draft in drafts:
+        draft_id = draft['id']
+        draft_msg = service.users().drafts().get(userId='me', id=draft_id).execute()
+        msg = draft_msg['message']
+        subject, sender, to, cc, bcc, date = extract_message_headers(msg)
+        print(f"Draft ID: {draft_id}, From: {sender}, Subject: {subject}, To: {to}, CC: {cc}, Bcc: {bcc}, Saved: {date}")
+
+
+def extract_message_headers(message):
+    subject = None
+    sender = None
+    to = None
+    cc = None
+    bcc = None
+    date = None
+
+    if message is not None:
+        for header in message['payload']['headers']:
             if header['name'].lower() == 'subject':
                 subject = header['value']
             if header['name'].lower() == 'from':
                 sender = header['value']
+            if header['name'].lower() == 'to':
+                to = header['value']
+            if header['name'].lower() == 'cc':
+                cc = header['value']
+            if header['name'].lower() == 'bcc':
+                bcc = header['value']
+            date = datetime.fromtimestamp(int(message['internalDate']) / 1000, timezone.utc).astimezone().strftime(
+            '%Y-%m-%d %H:%M:%S')
 
-        print(f"ID: {msg_id} From: {sender}, Subject: {subject}, Received: {date}")
+    return subject, sender, to, cc, bcc, date
+
+
+def fetch_email_or_draft(service, obj_id):
+    try:
+        # Try fetching as an email first
+        return service.users().messages().get(userId='me', id=obj_id, format='full').execute()
+    except HttpError as email_err:
+        if email_err.resp.status == 404 or email_err.resp.status == 400:
+            # If email not found, try fetching as a draft
+            draft_msg = service.users().drafts().get(userId='me', id=obj_id).execute()
+            return draft_msg['message']
+        else:
+            raise email_err  # Reraise the error if it's not a 404 (not found)
+
+
+def has_attachment(message):
+    def parse_parts(parts):
+        for part in parts:
+            if part['filename'] and part['body'].get('attachmentId'):
+                return True
+        return False
+
+    parts = message['payload'].get('parts', [])
+    if parts:
+        return parse_parts(parts)
+    else:
+        return False
+
+
+def get_email_body(message):
+    def parse_parts(parts):
+        for part in parts:
+            mime_type = part['mimeType']
+            if mime_type == 'text/plain' or mime_type == 'text/html':
+                body_data = part['body']['data']
+                decoded_body = base64.urlsafe_b64decode(body_data).decode('utf-8')
+                return decoded_body
+            if mime_type == 'multipart/alternative' or mime_type == 'multipart/mixed':
+                return parse_parts(part['parts'])
+        return None
+
+    try:
+        parts = message['payload'].get('parts', [])
+        if parts:
+            return parse_parts(parts)
+        else:
+            body_data = message['payload']['body']['data']
+            decoded_body = base64.urlsafe_b64decode(body_data).decode('utf-8')
+            return decoded_body
+    except Exception as e:
+        print(f'Error while decoding the email body: {e}')
+        return None
