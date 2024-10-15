cd5fee69c2f7eb25246c622da51314736f832f09
Nick Hale <4175918+njhale@users.noreply.github.com>
2024-10-15 15:53:18 -0400

    feat: add google search tool
    Add the Google Search tool.
    
    This is a modified version of answers-from-the-internet that responds
    with knowledge-like references to relevant search result page content
    instead of answering questions directly.
    
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

---
+++ google/search/src/search.ts
@@ -0,0 +1,49 @@
+import { BrowserContext } from '@playwright/test';
+import * as cheerio from 'cheerio';
+import { getMarkdown } from './getMarkdown.ts';
+
+export type SearchResult = {
+  url: string;
+  content: string;
+}
+
+export async function search(
+  context: BrowserContext,
+  noJSContext: BrowserContext,
+  query: string,
+  maxResults: number = 3 // Default to 3 results if not specified
+): Promise<SearchResult[]> {
+  const foundURLs = new Set<string>();
+  const contentsPromises: Array<Promise<SearchResult | null>> = [];
+
+  const page = await context.newPage();
+  const noJSPages = await Promise.all(
+    Array.from({ length: maxResults }, () => noJSContext.newPage()) 
+  );
+
+  try {
+    await page.goto(`https://www.google.com/search?q=${query}&udm=14`);
+    const content = await page.content();
+    const $ = cheerio.load(content);
+    const elements = $('#rso a[jsname]');
+  
+    elements.each((_, element) => {
+      if (contentsPromises.length >= maxResults) return false;
+
+      const url = $(element).attr('href') ?? '';
+      if (url && !url.includes('youtube.com/watch?v') && !foundURLs.has(url)) {
+        console.warn(url)
+        foundURLs.add(url);
+        contentsPromises.push(getMarkdown(noJSPages[contentsPromises.length], url).then(content => {
+          return content ? { url, content } : null;
+        }));
+      }
+    });
+
+    const results = (await Promise.all(contentsPromises)).filter(Boolean) as SearchResult[];
+    return results;
+  } finally {
+    await page.close();
+    await Promise.all(noJSPages.map(p => p.close()));
+  }
+}
