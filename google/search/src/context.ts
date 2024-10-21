8bb012f47b9da09cb614d87d9a567e3191278295
Nick Hale <4175918+njhale@users.noreply.github.com>
2024-10-21 15:53:20 -0400

    enhance: use system browser for google search
    Don't rely on post-install script to install chromium for google search.
    Instead, use whatever browser has already been installed on the system.
    Supports Chrome, Chromium, Firefox, and Edge.
    
    Signed-off-by: Nick Hale <4175918+njhale@users.noreply.github.com>

--- google/search/src/context.ts
+++ google/search/src/context.ts
@@ -1,4 +1,4 @@
-import { type BrowserContext, chromium } from '@playwright/test'
+import { type BrowserContext, chromium, firefox } from '@playwright/test'
 import { randomInt } from 'node:crypto'
 
 export interface ContextAndSessionDir {
@@ -6,20 +6,94 @@ export interface ContextAndSessionDir {
   sessionDir: string
 }
 
-export async function getNewContext (workspaceDir: string, javaScriptEnabled: boolean): Promise<ContextAndSessionDir> {
+export async function getNewContext(workspaceDir: string, javaScriptEnabled: boolean): Promise<ContextAndSessionDir> {
   const sessionDir = workspaceDir + '/afti_browser_session_' + randomInt(1, 1000000).toString()
   let context: BrowserContext
 
-  context = await chromium.launchPersistentContext(
-    sessionDir,
-    {
-      userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-      headless: true,
-      viewport: null,
-      args: ['--start-maximized', '--disable-blink-features=AutomationControlled'],
-      ignoreDefaultArgs: ['--enable-automation', '--use-mock-keychain'],
-      javaScriptEnabled
-    })
+  const browser = await getSystemBrowser()
+  switch (browser) {
+    case 'chromium':
+      context = await chromium.launchPersistentContext(
+        sessionDir,
+        {
+          userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+          headless: true,
+          viewport: null,
+          args: ['--start-maximized', '--disable-blink-features=AutomationControlled'],
+          ignoreDefaultArgs: ['--enable-automation', '--use-mock-keychain'],
+          javaScriptEnabled
+        })
+      break
+    case 'chrome':
+      context = await chromium.launchPersistentContext(
+        sessionDir,
+        {
+          userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+          headless: true,
+          viewport: null,
+          channel: 'chrome',
+          args: ['--start-maximized', '--disable-blink-features=AutomationControlled'],
+          ignoreDefaultArgs: ['--enable-automation', '--use-mock-keychain'],
+          javaScriptEnabled
+        })
+      break
+    case 'firefox':
+      context = await firefox.launchPersistentContext(
+        sessionDir,
+        {
+          userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Firefox/89.0 Safari/537.36',
+          headless: true,
+          viewport: null,
+          javaScriptEnabled
+        })
+      break
+    case 'edge':
+      context = await chromium.launchPersistentContext(
+        sessionDir,
+        {
+          userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36 Edg/91.0.864.64',
+          headless: true,
+          viewport: null,
+          channel: 'msedge',
+          args: ['--start-maximized', '--disable-blink-features=AutomationControlled'],
+          ignoreDefaultArgs: ['--enable-automation', '--use-mock-keychain'],
+          javaScriptEnabled
+        })
+      break
+    default:
+      throw new Error(`Unknown browser: ${browser}`)
+  }
 
   return { context, sessionDir }
+}
+
+let systemBrowser: string | undefined;
+
+async function getSystemBrowser(): Promise<string> {
+  if (systemBrowser) {
+    return systemBrowser
+  }
+
+  const browsers = [
+    { name: 'Chrome', launchFunction: async () => await chromium.launch({ channel: 'chrome' }) },
+    { name: 'Edge', launchFunction: async () => await chromium.launch({ channel: 'msedge' }) },
+    { name: 'Firefox', launchFunction: async () => await firefox.launch() },
+    { name: 'Chromium', launchFunction: async () => await chromium.launch() }
+  ]
+
+  const errors = []
+  for (const browser of browsers) {
+    try {
+      const browserInstance = await browser.launchFunction()
+      void browserInstance.close()
+
+      systemBrowser = browser.name.toLowerCase()
+
+      return browser.name.toLowerCase()
+    } catch (error) {
+      errors.push(error)
+    }
+  }
+
+  throw new Error(`No supported browsers (Chrome, Edge, Firefox) are installed. ${errors}`)
 }
