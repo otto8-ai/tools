7d95a94b38c200fa8cbc82315eb41c62b15fc1d1
Grant Linville <grant@acorn.io>
2024-10-29 09:19:40 -0400

    chore: reintroduce datasets (#130)
    Signed-off-by: Grant Linville <grant@acorn.io>

--- outlook/mail/pkg/commands/listMessages.go
+++ outlook/mail/pkg/commands/listMessages.go
@@ -3,12 +3,15 @@ package commands
 import (
 	"context"
 	"fmt"
+	"os"
 
+	"github.com/gptscript-ai/go-gptscript"
 	"github.com/gptscript-ai/tools/outlook/common/id"
 	"github.com/gptscript-ai/tools/outlook/mail/pkg/client"
 	"github.com/gptscript-ai/tools/outlook/mail/pkg/global"
 	"github.com/gptscript-ai/tools/outlook/mail/pkg/graph"
 	"github.com/gptscript-ai/tools/outlook/mail/pkg/printers"
+	"github.com/gptscript-ai/tools/outlook/mail/pkg/util"
 )
 
 func ListMessages(ctx context.Context, folderID string) error {
@@ -34,7 +37,36 @@ func ListMessages(ctx context.Context, folderID string) error {
 		return fmt.Errorf("failed to list mail: %w", err)
 	}
 
-	// TODO(g-linville): reintroduce datasets here once we can preserve the order of the elements
+	if len(messages) > 10 {
+		gptscriptClient, err := gptscript.NewGPTScript()
+		if err != nil {
+			return fmt.Errorf("failed to create GPTScript client: %w", err)
+		}
+		workspaceID := os.Getenv("GPTSCRIPT_WORKSPACE_ID")
+		dataset, err := gptscriptClient.CreateDataset(ctx, workspaceID, fmt.Sprintf("%s_outlook_mail", folderID), "Outlook mail messages in folder "+folderID)
+		if err != nil {
+			return fmt.Errorf("failed to create dataset: %w", err)
+		}
+		var elements []gptscript.DatasetElement
+		for _, message := range messages {
+			messageStr, err := printers.MessageToString(message, false)
+			if err != nil {
+				return fmt.Errorf("failed to convert message to string: %w", err)
+			}
+			elements = append(elements, gptscript.DatasetElement{
+				DatasetElementMeta: gptscript.DatasetElementMeta{
+					Name:        util.Deref(message.GetId()),
+					Description: util.Deref(message.GetSubject()),
+				},
+				Contents: messageStr,
+			})
+		}
+		if err := gptscriptClient.AddDatasetElements(ctx, workspaceID, dataset.ID, elements); err != nil {
+			return fmt.Errorf("failed to add dataset elements: %w", err)
+		}
+		fmt.Printf("Created dataset with ID %s with %d messages\n", dataset.ID, len(messages))
+		return nil
+	}
 
 	return printers.PrintMessages(messages, false)
 }
