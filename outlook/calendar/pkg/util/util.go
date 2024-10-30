4ea4e4ac4fa31d3bc3a37be54b4f7b248a130f7c
Grant Linville <grant@acorn.io>
2024-10-30 09:05:41 -0400

    Update outlook/calendar/pkg/util/util.go
    Co-authored-by: Donnie Adams <donald.g.adams@me.com>

--- outlook/calendar/pkg/util/util.go
+++ outlook/calendar/pkg/util/util.go
@@ -39,8 +39,8 @@ func MapValues[T comparable, U any](m map[T]U) []U {
 // Merge merges two maps where the values are arrays of the same type.
 // The resulting map will contain all keys from both maps, with each value containing the concatenated arrays from both maps.
 func Merge[T comparable, U any](one map[T][]U, two map[T][]U) map[T][]U {
-	out := make(map[T][]U)
-	checked := make(map[T]any)
+	out := make(map[T][]U, len(one) + len(two))
+	checked := make(map[T]struct{})
 
 	for k, v := range one {
 		checked[k] = struct{}{}
