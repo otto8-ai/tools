c3b5012f1d20efd72ac06cba35a7f3bbdfc7f1c7
Yingbei <yingbei@acorn.io>
2024-12-11 16:50:20 +0800

    add init jira implementation with a few functions.

---
+++ jira/src/dashboards.js
@@ -0,0 +1,75 @@
+import fetch from 'node-fetch'
+
+export async function getAllDashboard(baseUrl, auth) {
+    try {
+        const response = await fetch(`${baseUrl}/dashboard`, {
+            method: 'GET',
+            headers: {
+            'Authorization': auth,
+            'Accept': 'application/json'
+            }
+        })
+    
+        const data = await response.json()
+        console.log('Dashboards:')
+        data.dashboards.forEach(dashboard => {
+        const domain = new URL(dashboard.self).origin
+        console.log(`ID: ${dashboard.id}, Name: ${dashboard.name}, view-URL: ${domain}${dashboard.view}`)
+        })
+  
+      return data
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+}
+
+export async function getDashboard(baseUrl, auth, id) {
+    try {
+        const response = await fetch(`${baseUrl}/dashboard/${id}`, {
+        method: 'GET',
+        headers: {
+            'Authorization': auth,
+            'Accept': 'application/json'
+        }
+        })
+
+        const data = await response.json()
+        const domain = new URL(data.self).origin
+
+        console.log(`Got dashboard. ID: ${id}, Dashboard view-URL: ${domain}${data.view}, Content:\n${JSON.stringify(data, null, 2)}`)
+
+        return data
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+}
+
+
+export async function getDashboardGadgets(baseUrl, auth, dashboardId) {
+    try {
+        const response = await fetch(`${baseUrl}/dashboard/${dashboardId}/gadget`, {
+        method: 'GET',
+        headers: {
+            'Authorization': auth,
+            'Accept': 'application/json'
+        }
+        })
+
+        const data = await response.json()
+        console.log(`Got gadgets for dashboard ${dashboardId}: ${JSON.stringify(data, null, 2)}`)
+
+        return data
+    } catch (error) {
+        console.error(`Error fetching gadgets for dashboard ${dashboardId}:`, error)
+        throw error
+    }
+}
+
+  
+
+  
+
+
+  
